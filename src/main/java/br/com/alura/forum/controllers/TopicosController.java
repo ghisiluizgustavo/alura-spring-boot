c18a2d4f2adf02890cacedf52dd7bb4182a3e6ef
Luiz Gustavo Ghisi Flores <ghisiluizgustavo@gmail.com>
2020-12-14 23:26:02 -0300

    retornando 201 e location no header da req 🔑

--- src/main/java/br/com/alura/forum/controllers/TopicosController.java
+++ src/main/java/br/com/alura/forum/controllers/TopicosController.java
@@ -7,8 +7,11 @@ import br.com.alura.forum.models.TopicoDTO;
 import br.com.alura.forum.repositorys.CursoRepository;
 import br.com.alura.forum.repositorys.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -33,10 +36,12 @@ public class TopicosController {
     }
 
     @PostMapping()
-    public void cadastrar(@RequestBody TopicoForm topicoForm){
+    public ResponseEntity<TopicoDTO> cadastrar(@RequestBody TopicoForm topicoForm, UriComponentsBuilder uriBuilder){
 
         Topico topico = topicoForm.converter(cursoRepository);
 
         topicoRepository.save(topico);
+        URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
+        return ResponseEntity.created(uri).body(new TopicoDTO(topico));
     }
 }
