e7cfe63639ce8c5e6da2e627e9cef3b867995f04
Luiz Gustavo <>
2021-09-03 10:38:15 -0300

    adicionando perfis

--- src/main/java/br/com/alura/forum/config/security/AuthenticationByTokenFilter.java
+++ src/main/java/br/com/alura/forum/config/security/AuthenticationByTokenFilter.java
@@ -5,13 +5,11 @@ import br.com.alura.forum.repositorys.UserRepository;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
-import sun.plugin.liveconnect.SecurityContextHelper;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.swing.text.html.Option;
 import java.io.IOException;
 import java.util.Optional;
 
